fc82974909fb1a8e7e723d30d7593b3804134f1c
koreakshay <akshay23f96@gmail.com>
2025-12-08 21:46:39 -0600

    setup

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package httpserveringo
+package main
+
+import "net/http"
+
+func main() {
+	serveMux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: serveMux,
+	}
+	server.ListenAndServe()
+}
